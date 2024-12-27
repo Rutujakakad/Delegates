9f61038873328da6bbdacd93e3355242bdef5d90
67418_Rutuja <rutubkakad@gmail.com>
2024-12-27 08:04:32 +0530

    performed delagtes

---
+++ Delegates/Delegates/Delegates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Delegates
+    {
+        public delegate void PrintMessage (string message);
+        public static void PrintToConsole(string message)
+        {
+            Console.WriteLine("Console: " + message);
+        }
+
+        public static void PrintToFile(string message)
+        {
+            Console.WriteLine("File: " + message);
+        }
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using static Delegates.Delegates;
+namespace Delegates
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //  PrintMessage printMessageDel = new PrintMessage(PrintToConsole);
+            // printMessageDel("Delegates");
+
+
+            // for multicast delegate
+            PrintMessage printMessageDel = PrintToConsole;
+            printMessageDel += PrintToFile;
+            printMessageDel("Mulicast Delegate");
+        }
+    }
+}
